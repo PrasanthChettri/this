341e3d3f4b194fd62945fb7c6affffa70fed70f6
PrasanthChettri <prasanthchettri3@gmail.com>
2023-05-17 22:42:49 +0530

    fat

--- graph.py
+++ graph.py
@@ -13,12 +13,17 @@ def create_cluster_graph(S, CHs):
         for member in head.members:
             G.add_edge(head, member)
 
+    # Add edges between BS and CH nodes
+    for head in CHs:
+        G.add_edge("BS", head)
+
     # Create a layout for the graph
     pos = nx.spring_layout(G)
 
     # Draw nodes
     nx.draw_networkx_nodes(G, pos, nodelist=S, node_color='blue', alpha=0.5)
     nx.draw_networkx_nodes(G, pos, nodelist=CHs, node_color='red')
+    nx.draw_networkx_nodes(G, pos, nodelist=["BS"], node_color='green')
 
     # Draw edges
     nx.draw_networkx_edges(G, pos)
